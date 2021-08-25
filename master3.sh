70ae10a4315cfbe4d3c331b1b1b0e380189923ef
HaikelRH <hmejri@smartek.ae>
2021-08-25 19:25:11 +0400

    Create master3.sh

---
+++ master3.sh
@@ -0,0 +1,6 @@
+coreos-installer install /dev/sda
+--append-karg rd.neednet=1
+--append-karg ip=172.18.141.43::172.18.141.1:255.255.255.0:master3.scbocpuat.scb.local:ens3:none
+--append-karg nameserver=172.18.3.144
+--ignition-url http://172.18.141.30/master.ign
+--insecure-ignition
